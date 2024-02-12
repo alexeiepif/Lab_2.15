2360d3eaab42e320412cff9adf7319456a916a46
alexeiepif <aled2093746@gmail.com>
2024-02-12 20:04:09 +0300

    the second half of the examples

---
+++ program/prim11.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import os
+
+
+if __name__ == "__main__":
+    # creating a new directory with the name new
+    os.mkdir("Lab_2.15/program/new")
---
+++ program/prim14.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import os
+
+
+if __name__ == "__main__":
+    # removing the new directory
+    os.rmdir("Lab_2.15/program/new")
