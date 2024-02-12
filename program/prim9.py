6b355dea613b81c1aa3d6d9d888bf0cbf1afdbd1
alexeiepif <aled2093746@gmail.com>
2024-02-12 19:38:15 +0300

    prim9

---
+++ program/prim9.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import os
+
+
+if __name__ == "__main__":
+    # rename file2.txt to file3.txt
+    os.rename("Lab_2.15/program/file2.txt","Lab_2.15/program/file3.txt")
